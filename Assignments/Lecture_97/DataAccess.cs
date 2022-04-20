6c697f5f7a41e6ba2170eee31d2bdedba3a573d5
xumones <toastpork13@gmail.com>
2022-04-20 17:39:27 +0700

    Assignment_97_Update

---
+++ Assignments/Lecture_97/DataAccess.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace Assignment_97_Rawipote_P
+{
+    internal class DataAccess
+    {
+        public async static void InitializeDatabase()
+        {
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename=sqliteSample.db"))
+            {
+                db.Open();
+                String tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS Customers (UID INTEGER PRIMARY KEY, " +
+                    "first_Name CHARACTER(40) NULL)";
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+                createTable.ExecuteReader();
+            }
+        }
+        public static void AddData(string inputFirstName)
+        {
+            using (SqliteConnection db =
+              new SqliteConnection($"Filename=sqliteSample.db"))
+            {
+                db.Open();
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+                insertCommand.CommandText = "INSERT INTO Customers VALUES (NULL, @first_Name);";
+                insertCommand.Parameters.AddWithValue("@first_Name", inputFirstName);
+                insertCommand.ExecuteReader();
+                db.Close();
+            }
+        }
+        public static List<String> GetData()
+        {
+            List<String> entries = new List<string>();
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename=sqliteSample.db"))
+            {
+                db.Open();
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT first_Name from Customers", db);
+                SqliteDataReader query = selectCommand.ExecuteReader();
+                while (query.Read())
+                {
+                    entries.Add(query.GetString(0));
+                }
+                db.Close();
+            }
+            return entries;
+        }
+    }
+}
---
+++ Assignments/Lecture_97/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment_97_Rawipote_P
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataAccess.InitializeDatabase();
+        }
+
+        private void Add_Data_bt_Click(object sender, RoutedEventArgs e)
+        {
+            DataAccess.AddData(Text_input.Text);
+            MessageBox.Show(Text_input.Text + " has been add into your list.");
+        }
+
+        private void Show_Data_bt_Click(object sender, RoutedEventArgs e)
+        {
+            string Dataresult = "";
+            foreach (string data in DataAccess.GetData())
+            {
+                Dataresult = Dataresult + data + "\n";
+            }
+            MessageBox.Show(Dataresult);
+        }
+
+        private void Text_input_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Text_input.Text = "";
+        }
+    }
+}
